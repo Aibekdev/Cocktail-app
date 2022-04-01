197a9d4bb88a2e5d0bc457c74ae6b70c440ec91c
Aibekbjj <aibekon@mail.ru>
2022-04-01 18:56:50 +0600

    Initial Commit

---
+++ Cocktail app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktail app
+//
+//  Created by Aibek on 01.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
